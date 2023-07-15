fc179922f3ad433795142c6bd5870f9d9a8b13f3
kim <se6816@naver.com>
2023-07-16 01:02:46 +0900

    test

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
-
     @GetMapping("/test")
     public String test() {
         return "Hello World!!";
